6e993e2cb1e42541fad0a9af9a185621b32fa09e
Satyam <satyamraiakr464@gmail.com>
2021-04-21 15:26:28 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+
 import './App.css';
 
+
+const lightCountdown = [1000, 2000, 3000];
+
 function App() {
+
+  const [colorIndex, setcolorIndex] = useState(0);
+
+  useEffect(() => {
+    console.log("use Effect")
+    setTimeout(() => {
+      let colorIndex2 = (colorIndex + 1) % 3;
+      setcolorIndex(colorIndex2)
+    }, lightCountdown[colorIndex]);
+
+
+
+
+  }, [colorIndex])
+
+  //console.log(lightCountdown[colorIndex])
+
+  // const incr = ()=> {
+  //   let colorIndex2 = (colorIndex + 1) % 3;
+  //   setcolorIndex( colorIndex2)
+  //   //console.log(colorIndex  , colorIndex2);
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="signal">
+        {/* <button type="button" onClick={incr}></button> */}
+        <Light className="red light" color="red" active={colorIndex === 2 } />
+        <Light className="orange light" color="orange" active={colorIndex === 1 } />
+        <Light className="green light" color="green" active={colorIndex === 0 } />
+      </div>
+
     </div>
+
   );
+
 }
 
+
+const Light = ({ color, active }) => (
+  <div
+    className={color}
+    style={{ backgroundColor: color, opacity: active ? 1 : 0.2 }}
+  />
+)
+
 export default App;
+
+
+
+
+// const [colorIndex, setColorIndex] = useState(0);
+
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setColorIndex((colorIndex + 1) % 3);
+//     }, lightDuration[colorIndex]);
+//     console.log(timer)
+//     console.log(colorIndex)
+//     return () => {
+//       clearTimeout(timer);
+//     }
+
+//   },[colorIndex])
+
+
+
+
+
+
+// const Light = ({ color, active }) => (
+//   <div 
+//       className="light"
+//       style={{ backgroundColor:color , opacity: active ? 1 : 0.2}}
+//   />
+// )
